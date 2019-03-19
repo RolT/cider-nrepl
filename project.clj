23604daa9a02e692727b014a1b95c7f75fdb72f2
Benedek Fazekas <benedek.fazekas@gmail.com>
2019-03-19 10:31:06 +0000

    Upgrade mranderson to latests snapshot to test build
    mranderson 0.5.x specific config added to the project file as
    well. Testing shadowing only for now. Shadowing only means that
    mranderson works with a resolved dependency tree and does not create a
    deeply nested directory structure, only adds prefixes to all
    dependencies. Handles all depedendencies as first level ones and
    processes them in a topological order.

--- project.clj
+++ project.clj
@@ -13,14 +13,14 @@
                  ^:source-dep [compliment "0.3.8"]
                  ^:source-dep [cljs-tooling "0.3.1"]
                  ^:source-dep [cljfmt "0.6.4" :exclusions [org.clojure/clojurescript]]
-                 ;; Not used directly in cider-nrepl, but needed because of tools.namespace
-                 ;; and the way MrAnderson processes dependencies
-                 ;; See https://github.com/clojure-emacs/cider/issues/2176 for details
-                 ^:source-dep [org.clojure/java.classpath "0.3.0"]
                  ^:source-dep [org.clojure/tools.namespace "0.3.0-alpha4"]
                  ^:source-dep [org.clojure/tools.trace "0.7.10"]
                  ^:source-dep [org.clojure/tools.reader "1.2.2"]]
-  :plugins [[thomasa/mranderson "0.4.9"]]
+  :plugins [[thomasa/mranderson "0.5.0-SNAPSHOT"]]
+  :mranderson {:overrides   {[mvxcvi/puget fipp] [fipp "0.6.15"]}; only takes effect if shadowing-only is false, which case [fipp "0.6.15"] can be removed from th dependencies vector
+               :expositions [[mvxcvi/puget fipp]]; only takes effect if shadowing-only is false
+               :shadowing-only true}
+
   :exclusions [org.clojure/clojure]
 
   :filespecs [{:type :bytes :path "cider/cider-nrepl/project.clj" :bytes ~(slurp "project.clj")}]
